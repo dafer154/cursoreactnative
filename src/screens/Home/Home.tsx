a55c4f8fdfad6e9c6e5e124c154797f057dbe5e3
davidzul <david.zuluaga@correounivalle.edu.co>
2021-08-31 21:41:46 -0500

    include flatlist and activity indicator

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,50 +1,86 @@
 import React, { useEffect, useState } from 'react';
-import { View, Alert, SafeAreaView, ScrollView } from 'react-native';
-import { Typography, DefaultButton, Separator } from '../../components';
+import { View, Alert, ActivityIndicator, FlatList } from 'react-native';
+import { Typography, DefaultButton, Separator, Header } from '../../components';
 import styles from './styles';
 import { goToScreen } from '../../navigation/controls';
-
+import { useNetInfo } from '@react-native-community/netinfo';
 import { getAllBooks } from '../../services';
+import { colors } from '../../utils/theme';
 
 const goToExperimentalScreen = () => {
   goToScreen('Experimental');
 };
 
+const ListItem = ({ title }: { title: string }) => (
+  <View style={styles.listItemContainer}>
+    <Typography>{title}</Typography>
+  </View>
+);
+
+// @ts-ignore
+const renderFlatlistItem = ({ item }) => <ListItem title={item.title} />;
+
 const HomeScreen = () => {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const netInfo = useNetInfo();
+
+  const getBooksData = async () => {
+    setLoading(true);
+    try {
+      const { success, data } = await getAllBooks();
+      if (success) {
+        setBooks(data);
+      } else {
+        Alert.alert('Error getting books on Home Screen');
+      }
+    } catch (error) {
+      console.log('Error getting books on Home Screen', error);
+      Alert.alert('Error getting books on Home Screen');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const dis = async () => {
-      try {
-        const { success, data } = await getAllBooks();
-        if (success) {
-          setBooks(data);
-        } else {
-          Alert.alert('Error getting on');
-        }
-      } catch (error) {
-        console.log('Error', error);
-        Alert.alert('Error getting on');
-      }
-    };
-    dis();
+    getBooksData();
   }, []);
 
-  console.log('Inside HomeScreen');
+  if (!netInfo.isConnected) {
+    return (
+      <View style={styles.wholeScreenCenter}>
+        <Typography size={20}>You don't have internet :'(</Typography>
+      </View>
+    );
+  }
+
+  if (loading) {
+    return (
+      <>
+        <Header showBackButton={false} title="Home Screen" />
+        <View style={styles.wholeScreenCenter}>
+          <ActivityIndicator size="large" color={colors.mainOrange} />
+        </View>
+      </>
+    );
+  }
 
   return (
     <>
-      <SafeAreaView />
-      <ScrollView style={styles.scrollView}>
-        <View style={styles.mainContainer}>
-          <Typography size={20} variant="bold">
-            Home Screen
-          </Typography>
-          <Separator separation={10} />
-          <DefaultButton text="Go To Experimental Screen" onPress={goToExperimentalScreen} />
-          <Typography>{JSON.stringify(books, null, 2)}</Typography>
-        </View>
-      </ScrollView>
+      <Header showBackButton={false} title="Home Screen" />
+      <View style={styles.mainContainer}>
+        <Separator separation={10} />
+        <DefaultButton text="Go To Experimental Screen" onPress={goToExperimentalScreen} />
+        <Separator separation={10} />
+        <FlatList
+          refreshing={loading}
+          onRefresh={getBooksData}
+          data={books}
+          renderItem={renderFlatlistItem}
+          style={styles.flatList}
+        />
+      </View>
     </>
   );
 };
